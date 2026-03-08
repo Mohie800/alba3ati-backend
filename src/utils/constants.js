ed458590d3e2a243294ee81264a950c30ad89281
mohie800 <mohy800@gmail.com>
2026-03-08 05:45:29 +0300

    feat: update quick play grace period to 15 seconds

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,6 +15,6 @@ exports.GRACE_PERIOD = 60;
 // Quick Play
 exports.QUICK_PLAY_MIN_PLAYERS = 10;
 exports.QUICK_PLAY_MAX_PLAYERS = 20;
-exports.QUICK_PLAY_COUNTDOWN = 10;       // seconds
-exports.QUICK_PLAY_GRACE_PERIOD = 10;    // seconds (vs 60 for normal)
+exports.QUICK_PLAY_COUNTDOWN = 10; // seconds
+exports.QUICK_PLAY_GRACE_PERIOD = 15; // seconds (vs 60 for normal)
 exports.QUICK_PLAY_DISCUSSION_TIME = 180; // 3 minutes in seconds
